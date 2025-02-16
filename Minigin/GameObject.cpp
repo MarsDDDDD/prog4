16d22efc97cfe4b831daa3d11e9042bb2224c7c8
a <marsdding@gmail.com>
2025-02-16 17:06:37 +0100

    object-component system

--- Minigin/GameObject.cpp
+++ Minigin/GameObject.cpp
@@ -1,3 +1,4 @@
+
 #include <string>
 #include "GameObject.h"
 #include "BaseComponent.h" // Include the BaseComponent header
@@ -33,53 +34,4 @@ void dae::GameObject::Render() const
 void dae::GameObject::SetPosition(float x, float y)
 {
 	m_transform.SetPosition(x, y, 0.0f);
-}
-
-template <typename T>
-void dae::GameObject::AddComponent(std::shared_ptr<T> component)
-{
-	// Check if a component of this type already exists
-	if (HasComponent<T>())
-	{
-		return; // Or throw an exception, depending on desired behavior
-	}
-
-	m_components.push_back(component);
-	m_componentMap[typeid(T)] = component;
-	component->SetGameObject(this); // Important: Set the GameObject pointer
-}
-
-template <typename T>
-std::shared_ptr<T> dae::GameObject::GetComponent() const
-{
-	auto it = m_componentMap.find(typeid(T));
-	if (it != m_componentMap.end()) {
-		// Cast to the requested type using std::dynamic_pointer_cast.  Safer than static_cast.
-		return std::dynamic_pointer_cast<T>(it->second);
-	}
-	return nullptr; // Or consider throwing an exception if it *should* exist.
-}
-
-template <typename T>
-bool dae::GameObject::HasComponent() const
-{
-	return m_componentMap.count(typeid(T)) > 0;
-}
-
-template <typename T>
-void dae::GameObject::RemoveComponent()
-{
-	auto typeIndex = typeid(T);
-	if (m_componentMap.count(typeIndex) > 0)
-	{
-		auto component = m_componentMap[typeIndex];
-		// Remove from the vector
-		auto it = std::find(m_components.begin(), m_components.end(), component);
-		if (it != m_components.end())
-		{
-			m_components.erase(it);
-		}
-
-		m_componentMap.erase(typeIndex);
-	}
 }
--- Minigin/GameObject.h
+++ Minigin/GameObject.h
@@ -1,3 +1,4 @@
+
 #pragma once
 #include <memory>
 #include <vector>
@@ -5,6 +6,7 @@
 #include <typeindex> // Required for type_index
 #include <map>
 #include "Transform.h"
+#include "BaseComponent.h"
 
 namespace dae
 {
@@ -47,4 +49,54 @@ namespace dae
 		std::vector<std::shared_ptr<BaseComponent>> m_components; // Store base class pointers
 		std::map<std::type_index, std::shared_ptr<BaseComponent>> m_componentMap;
 	};
+
+	// Put the template function definitions *in the header file*.
+	template <typename T>
+	void GameObject::AddComponent(std::shared_ptr<T> component)
+	{
+		// Check if a component of this type already exists
+		if (HasComponent<T>())
+		{
+			return; // Or throw an exception, depending on desired behavior
+		}
+
+		m_components.push_back(component);
+		m_componentMap[typeid(T)] = component;
+		component->SetGameObject(this); // Important: Set the GameObject pointer
+	}
+
+	template <typename T>
+	std::shared_ptr<T> GameObject::GetComponent() const
+	{
+		auto it = m_componentMap.find(typeid(T));
+		if (it != m_componentMap.end()) {
+			// Cast to the requested type using std::dynamic_pointer_cast.  Safer than static_cast.
+			return std::dynamic_pointer_cast<T>(it->second);
+		}
+		return nullptr; // Or consider throwing an exception if it *should* exist.
+	}
+
+	template <typename T>
+	bool GameObject::HasComponent() const
+	{
+		return m_componentMap.count(typeid(T)) > 0;
+	}
+
+	template <typename T>
+	void GameObject::RemoveComponent()
+	{
+		auto typeIndex = typeid(T);
+		if (m_componentMap.count(typeIndex) > 0)
+		{
+			auto component = m_componentMap[typeIndex];
+			// Remove from the vector
+			auto it = std::find(m_components.begin(), m_components.end(), component);
+			if (it != m_components.end())
+			{
+				m_components.erase(it);
+			}
+
+			m_componentMap.erase(typeIndex);
+		}
+	}
 }
