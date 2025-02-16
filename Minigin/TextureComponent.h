cf171595c20bf7400a81137ba15e1ebfc1cab7f5
a <marsdding@gmail.com>
2025-02-16 17:49:47 +0100

    added back background and logo

---
+++ Minigin/TextureComponent.h
@@ -0,0 +1,27 @@
+// ===== TextureComponent.h =====
+#pragma once
+#include "BaseComponent.h"
+#include <string>
+#include <memory>
+#include "Texture2D.h"
+
+namespace dae
+{
+	class TextureComponent : public BaseComponent
+	{
+	public:
+		TextureComponent(std::shared_ptr<Texture2D> texture);
+		~TextureComponent() override = default;
+
+		void Render() const override;
+		void SetTexture(std::shared_ptr<Texture2D> texture);
+
+		// Optional: Add methods to control scaling, rotation, etc. if needed.
+		void SetDimensions(float width, float height);
+		const glm::vec2& GetDimensions() const { return m_dimensions; };
+
+	private:
+		std::shared_ptr<Texture2D> m_texture;
+		glm::vec2 m_dimensions{};
+	};
+}
