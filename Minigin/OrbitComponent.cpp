ae979dc22533a720b69a2ec52123564e5c3cdf73
a <marsdding@gmail.com>
2025-02-21 15:51:22 +0100

    Debugged OrbitComponent.

--- Minigin/OrbitComponent.cpp
+++ Minigin/OrbitComponent.cpp
@@ -1,18 +1,30 @@
 #include "OrbitComponent.h"
 #include "GameObject.h"
 #include <gtc/constants.hpp>
+#include <iostream> // For debug logging
 
 dae::OrbitComponent::OrbitComponent(GameObject* pOwner, GameObject* pTarget, float radius, float speed)
     : BaseComponent(pOwner), m_pTarget(pTarget), m_Radius(radius), m_Speed(speed), m_Angle(0.0f)
 {
+    if (!m_pTarget)
+    {
+        std::cerr << "OrbitComponent: Target GameObject is null!" << std::endl;
+    }
 }
 
 void dae::OrbitComponent::Update(float deltaTime)
 {
-
-    if (m_pTarget == nullptr)
+    if (!m_pTarget)
     {
-        return; // Early exit if the target is null
+        std::cerr << "OrbitComponent: Target GameObject is null during update!" << std::endl;
+        return;
+    }
+
+    auto targetTransform = m_pTarget->GetTransform();
+    if (!targetTransform)
+    {
+        std::cerr << "OrbitComponent: Target TransformComponent is null during update!" << std::endl;
+        return;
     }
 
     m_Angle += m_Speed * deltaTime;
@@ -21,8 +33,8 @@ void dae::OrbitComponent::Update(float deltaTime)
         m_Angle -= glm::two_pi<float>();
     }
 
-    float x = m_pTarget->GetTransform()->GetLocalPosition().x + m_Radius * cos(m_Angle);
-    float y = m_pTarget->GetTransform()->GetLocalPosition().y + m_Radius * sin(m_Angle);
+    float x = targetTransform->GetLocalPosition().x + m_Radius * cos(m_Angle);
+    float y = targetTransform->GetLocalPosition().y + m_Radius * sin(m_Angle);
 
     GetGameObject()->GetTransform()->SetLocalPosition(x, y, 0.0f);
 }
