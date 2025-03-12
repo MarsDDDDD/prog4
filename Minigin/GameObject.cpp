21a77cbba258895540492e4f61b401b866351d91
Shengyan Ding <70955483+MarsDDDDD@users.noreply.github.com>
2025-03-12 13:49:36 -0700

    add functionalities

--- Minigin/GameObject.cpp
+++ Minigin/GameObject.cpp
@@ -7,35 +7,58 @@ namespace dae
 {
     GameObject::GameObject()
     {
-        // Create TransformComponent with unique ownership
-        auto transform = std::make_unique<TransformComponent>(this);
-        AddComponent(std::move(transform));
     }
 
     GameObject::~GameObject() = default;
 
 
-	void GameObject::Update()
+	void GameObject::Update(float deltaTime)
 	{
 		for (const auto& component : m_Components)
 		{
-			component->Update();
+			component->Update(deltaTime);
 		}
 		for (const auto& child : m_pChildren)
 		{
-			child->Update();
+			child->Update(deltaTime);
 		}
+
+		// Apply pending removals
+		for (auto* componentToRemove : m_pComponentsToRemove)
+		{
+			// Find this pointer in m_Components
+			auto it = std::find_if(
+				m_Components.begin(),
+				m_Components.end(),
+				[componentToRemove](std::unique_ptr<BaseComponent>& c) {
+					return c.get() == componentToRemove;
+				}
+			);
+
+			// Remove from vector if found
+			if (it != m_Components.end())
+			{
+				m_Components.erase(it);
+			}
+
+			// Also remove from the lookup map
+			auto typeIndex = m_ComponentTypesToRemove.front();
+			m_ComponentMap.erase(typeIndex);
+			m_ComponentTypesToRemove.pop();
+		}
+		m_pComponentsToRemove.clear();
+
 	}
 
-	void GameObject::FixedUpdate()
+	void GameObject::FixedUpdate(float deltaTime)
 	{
-		for (const auto& component : m_Components)
+		for (auto& component : m_Components)
 		{
-			component->FixedUpdate();
+			component->FixedUpdate(deltaTime);
 		}
 		for (const auto& child : m_pChildren)
 		{
-			child->FixedUpdate();
+			child->FixedUpdate(deltaTime);
 		}
 	}
 
@@ -51,8 +74,20 @@ namespace dae
 		}
 	}
 
-	void GameObject::SetParent(GameObject* pParent)
+	void GameObject::SetLocalPosition(float x, float y)
 	{
+		// Directly call the TransformComponent since it's always present
+		GetTransform()->SetLocalPosition(x, y, 0.0f);
+	}
+
+	void dae::GameObject::SetParent(GameObject* pParent, bool keepWorldPosition)
+	{
+		// Prevent self-parenting, setting same parent or cyclical references
+		if (pParent == this || pParent == m_pParent || (pParent && pParent->IsDescendant(this)))
+		{
+			return;
+		}
+
 		GameObject* pOldParent{};
 
 		if (pOldParent)
@@ -81,23 +116,20 @@ namespace dae
 		m_pParent = pParent;
 
 		if (pParent)
-			pParent->m_pChildren.push_back(std::unique_ptr<GameObject>(this));
+			pParent->m_pChildren.emplace_back(std::unique_ptr<GameObject>(this));
 
 		auto pTransform{ GetTransform() };
-		if (!pTransform)
-			return;
 
-		if (pParent)
+		if (pParent && keepWorldPosition)
 		{
-			// Set the local position to the position relative to the new parent
-			auto pParentTransform{ pParent->GetTransform() };
-			if (pParentTransform)
-				pTransform->SetLocalPosition(pTransform->GetWorldPosition() - pParentTransform->GetWorldPosition());
+			glm::vec3 originalWorldPosition{ GetTransform()->GetWorldPosition() };
+			glm::vec3 parentWorldPosition = pParent->GetTransform()->GetWorldPosition();
+			glm::vec3 newLocalPosition = originalWorldPosition - parentWorldPosition;
+			GetTransform()->SetLocalPosition(newLocalPosition);
 		}
 		else
 		{
-			// Set the local position to the world position
-			pTransform->SetLocalPosition(pTransform->GetWorldPosition());
+			GetTransform()->SetPositionDirty();
 		}
 
 	}
@@ -123,13 +155,30 @@ namespace dae
 	GameObject* GameObject::CreateGameObject()
 	{
 		auto pGameObject{ std::make_unique<GameObject>() };
-		pGameObject->Init();
+		// Create TransformComponent with unique ownership
+		auto transform = std::make_unique<TransformComponent>(this);
+		AddComponent(std::move(transform));
 
-		const auto pGameObjectPtr{ pGameObject.get() };
+		const auto pGameObjectToReturn{ pGameObject.get() };
 
 		pGameObject->m_pParent = this;
-		m_pChildren.push_back(std::move(pGameObject));
+		m_pChildren.emplace_back(std::move(pGameObject));
 
-		return pGameObjectPtr;
+		return pGameObjectToReturn;
+	}
+
+
+	bool GameObject::IsDescendant(GameObject* potentialDescendant) const
+	{
+		auto current = potentialDescendant->m_pParent;
+		while (current)
+		{
+			if (current == this)
+			{
+				return true;
+			}
+			current = current->m_pParent;
+		}
+		return false;
 	}
 }
