fc3a97a749affe1b9d3c36ee31292d33af7cf685
a <marsdding@gmail.com>
2025-03-28 05:36:28 +0100

    Moved game files to the game project

--- Minigin/Command.h
+++ Minigin/Command.h
@@ -1,21 +1,4 @@
 #pragma once
-//#pragma once
-//
-//namespace dae
-//{
-//	class Command
-//	{
-//	public:
-//		explicit Command() = default;
-//		virtual ~Command() = default;
-//		Command(const Command& other) = delete;
-//		Command& operator=(const Command& other) = delete;
-//		Command(Command&& other) = delete;
-//		Command& operator=(Command&& other) = delete;
-//
-//		virtual void Execute(float deltaTime = 0.f) = 0;
-//	};
-//}
 #include "GameObject.h"
 #include "Observer.h"
 
@@ -33,48 +16,4 @@ namespace dae
 
 		virtual void Execute(float deltaTime = 0.f) = 0;
 	};
-
-	//enum class Direction
-	//{
-	//	Up,
-	//	Down,
-	//	Left,
-	//	Right
-	//};
-
-	//class MoveCommand final : public Command
-	//{
-	//public:
-	//	explicit MoveCommand(GameObject* pGameObject, float speed, Direction direction)
-	//		: m_pGameObject{ pGameObject }
-	//		, m_Speed{ speed }
-	//		, m_Direction{ direction }
-	//	{
-	//	}
-
-	//	void Execute(float deltaTime) override;
-
-	//private:
-	//	GameObject* m_pGameObject{};
-	//	float m_Speed{};
-	//	Direction m_Direction{};
-	//};
-
-	//class DebugEventCommand final : public Command
-	//{
-	//public:
-	//	explicit DebugEventCommand(GameObject* pGameObject, int amount, Observer::EventId EventId)
-	//		: m_pGameObject{ pGameObject }
-	//		, m_Amount{ amount }
-	//		, m_EventId{ EventId }
-	//	{
-	//	}
-
-	//	void Execute(float deltaTime) override;
-
-	//private:
-	//	GameObject* m_pGameObject{};
-	//	int m_Amount{};
-	//	Observer::EventId m_EventId{};
-	//};
 }
